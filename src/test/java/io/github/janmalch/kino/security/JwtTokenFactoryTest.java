a19aabdf3c7272ee783f896420714626666ccf82
bko <koll.bernhard@gmail.com>
2019-04-05 22:19:34 +0200

    test: refactor test

--- src/test/java/io/github/janmalch/kino/security/JwtTokenFactoryTest.java
+++ src/test/java/io/github/janmalch/kino/security/JwtTokenFactoryTest.java
@@ -3,6 +3,7 @@ package io.github.janmalch.kino.security;
 import static org.junit.jupiter.api.Assertions.*;
 
 import io.github.janmalch.kino.entity.Account;
+import java.util.concurrent.TimeUnit;
 import org.jose4j.jwt.MalformedClaimException;
 import org.jose4j.jwt.consumer.InvalidJwtException;
 import org.junit.jupiter.api.Test;
@@ -21,13 +22,29 @@ class JwtTokenFactoryTest {
   }
 
   @Test
-  void parse() throws MalformedClaimException, InvalidJwtException {
+  void parseAndReturnSameToken() throws MalformedClaimException, InvalidJwtException {
     JwtTokenFactory factory = new JwtTokenFactory();
     Account acc = new Account();
     acc.setEmail("TestUser@mail.de");
     Token token = factory.generateToken(acc.getEmail());
     Token newToken = factory.parse(token.getTokenString());
-    assertNotEquals(token, newToken);
-    assertTrue(newToken.isExpired());
+    assertEquals(token.getSubject(), newToken.getSubject());
+    assertEquals(token.getTokenString(), newToken.getTokenString());
+    assertEquals(token.isExpired(), newToken.isExpired());
+    assertFalse(newToken.isExpired());
+  }
+
+  @Test
+  void parseAndReturnExpiredToken() {
+    JwtTokenFactory factory = new JwtTokenFactory();
+    factory.setTokenDuration(TimeUnit.MILLISECONDS.toMillis(1));
+    Account acc = new Account();
+    acc.setEmail("TestUser@mail.de");
+    Token token = factory.generateToken(acc.getEmail());
+    assertThrows(
+        InvalidJwtException.class,
+        () -> {
+          factory.parse(token.getTokenString());
+        });
   }
 }
