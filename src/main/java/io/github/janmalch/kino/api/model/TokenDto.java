3b6d8d183b46ff5869acaa578652cfb4974baf25
JanMalch <janmalch@gmail.com>
2019-04-05 23:02:17 +0200

    feat: added login use-case

---
+++ src/main/java/io/github/janmalch/kino/api/model/TokenDto.java
@@ -0,0 +1,14 @@
+package io.github.janmalch.kino.api.model;
+
+public class TokenDto {
+
+  private String token;
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+}
