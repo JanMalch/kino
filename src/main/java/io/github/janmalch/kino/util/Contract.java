917749cbafe26d653255f448ff5472a348de7891
JanMalch <janmalch@gmail.com>
2019-04-05 15:24:32 +0200

    style: removed the declaration of thrown runtime exception

--- src/main/java/io/github/janmalch/kino/util/Contract.java
+++ src/main/java/io/github/janmalch/kino/util/Contract.java
@@ -48,7 +48,7 @@ public final class Contract {
    * @param message description of what is expected
    * @throws IllegalArgumentException if the <code>value</code> argument is false
    */
-  public static void require(boolean value, String message) throws IllegalArgumentException {
+  public static void require(boolean value, String message) {
     if (!value) {
       throw new IllegalArgumentException(message);
     }
@@ -61,7 +61,7 @@ public final class Contract {
    * @param message description of what is expected
    * @throws IllegalStateException if the <code>value</code> argument is false
    */
-  public static void check(boolean value, String message) throws IllegalStateException {
+  public static void check(boolean value, String message) {
     if (!value) {
       throw new IllegalStateException(message);
     }
