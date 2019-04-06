826164a0965d9813b647bddf952001b98ab284a5
bko <koll.bernhard@gmail.com>
2019-04-06 18:24:01 +0200

    test: test getAllChildren()

---
+++ src/test/java/io/github/janmalch/kino/entity/RoleTest.java
@@ -0,0 +1,17 @@
+package io.github.janmalch.kino.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class RoleTest {
+  @Test
+  void getAllChildren() {
+    List<Role> expectedList = new ArrayList<>();
+    expectedList.add(Role.ADMIN);
+    List<Role> actual = Role.MODERATOR.getAllChildren();
+    assertEquals(actual, expectedList);
+  }
+}
