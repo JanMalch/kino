d26029d2c5293674fd023d64195f89a3e8488fb2
bko <koll.bernhard@gmail.com>
2019-04-07 15:04:16 +0200

    fix: add hasMinRole() Test

--- src/test/java/io/github/janmalch/kino/entity/RoleTest.java
+++ src/test/java/io/github/janmalch/kino/entity/RoleTest.java
@@ -1,26 +1,28 @@
 package io.github.janmalch.kino.entity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RoleTest {
   @Test
-  void getAllChildren() {
-    List<Role> expectedList = new ArrayList<>();
-    expectedList.add(Role.ADMIN);
-    List<Role> actual = Role.MODERATOR.getAllChildren();
-    assertEquals(actual, expectedList);
-  }
+  void hasMinRole() {
+    Role testRole = Role.ADMIN;
+    assertTrue(testRole.hasMinRole(Role.GUEST));
+    assertTrue(testRole.hasMinRole(Role.CUSTOMER));
+    assertTrue(testRole.hasMinRole(Role.MODERATOR));
+    assertTrue(testRole.hasMinRole(Role.ADMIN));
 
-  @Test
-  void getAllChildren2() {
-    List<Role> expectedList = new ArrayList<>();
-    expectedList.add(Role.MODERATOR);
-    expectedList.add(Role.ADMIN);
-    List<Role> actual = Role.CUSTOMER.getAllChildren();
-    assertEquals(actual, expectedList);
+    testRole = Role.CUSTOMER;
+    assertTrue(testRole.hasMinRole(Role.GUEST));
+    assertTrue(testRole.hasMinRole(Role.CUSTOMER));
+    assertFalse(testRole.hasMinRole(Role.MODERATOR));
+    assertFalse(testRole.hasMinRole(Role.ADMIN));
+
+    testRole = Role.GUEST;
+    assertTrue(testRole.hasMinRole(Role.GUEST));
+    assertFalse(testRole.hasMinRole(Role.CUSTOMER));
+    assertFalse(testRole.hasMinRole(Role.MODERATOR));
+    assertFalse(testRole.hasMinRole(Role.ADMIN));
   }
 }
