ebd83144a03f4fd95eea53b424766cb7d0918e10
bko <koll.bernhard@gmail.com>
2019-04-13 20:20:11 +0200

    fix: making methods public, edit exception

--- src/main/java/io/github/janmalch/kino/security/PasswordManager.java
+++ src/main/java/io/github/janmalch/kino/security/PasswordManager.java
@@ -7,33 +7,36 @@ import java.security.SecureRandom;
 
 public class PasswordManager {
 
-  byte[] generateSalt() {
+  public byte[] generateSalt() {
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
     random.nextBytes(salt);
     return salt;
   }
 
-  HashedPassword hashPassword(String clearPassword, byte[] salt) {
-    String generatedHashedPassword = "";
+  public String hashPassword(String clearPassword, byte[] salt) {
+    String generatedHashedPassword;
+    MessageDigest md;
     try {
-      MessageDigest md = MessageDigest.getInstance("SHA-512");
+      md = MessageDigest.getInstance("SHA-512");
 
-      md.update(salt);
-      byte[] bytes = md.digest(clearPassword.getBytes(StandardCharsets.UTF_8));
-      StringBuilder sb = new StringBuilder();
-      // https://stackoverflow.com/questions/36491665/byte-to-integer-and-then-to-string-conversion-in-java
-      for (int i = 0; i < bytes.length; i++) {
-        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-      }
-      generatedHashedPassword = sb.toString();
     } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-    return new HashedPassword(generatedHashedPassword, salt);
+    assert md != null;
+    md.update(salt);
+    byte[] bytes = md.digest(clearPassword.getBytes(StandardCharsets.UTF_8));
+    StringBuilder sb = new StringBuilder();
+    // https://stackoverflow.com/questions/36491665/byte-to-integer-and-then-to-string-conversion-in-java
+    for (int i = 0; i < bytes.length; i++) {
+      sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+    }
+    generatedHashedPassword = sb.toString();
+
+    return generatedHashedPassword;
   }
 
-  boolean isSamePassword(String clearPassword, String hash, byte[] salt) {
+  public boolean isSamePassword(String clearPassword, String hash, byte[] salt) {
     return hash.equals(hashPassword(clearPassword, salt));
   }
 }
