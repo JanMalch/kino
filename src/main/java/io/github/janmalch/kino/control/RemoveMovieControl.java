a9f34406a61f08d9372def8aa54e62c23450026b
JanMalch <janmalch@gmail.com>
2019-04-14 21:15:34 +0200

    fix: fixed bug in RemoveMovieControl

--- src/main/java/io/github/janmalch/kino/control/RemoveMovieControl.java
+++ src/main/java/io/github/janmalch/kino/control/RemoveMovieControl.java
@@ -18,11 +18,18 @@ public class RemoveMovieControl implements Control<Void> {
   public <T> T execute(ResultBuilder<T, Void> result) {
     var movie = repository.find(id);
     if (movie == null) {
-      return result.failure(Problem.builder(Response.Status.NOT_FOUND).instance().build());
+      return result.failure(
+          Problem.builder()
+              .type("movie/not-found")
+              .status(Response.Status.NOT_FOUND)
+              .title("Unable to find movie with the given ID")
+              .detail("No movie found for ID " + id)
+              .instance()
+              .build());
     }
 
     var affected = repository.remove(movie);
-    Contract.affirm(affected != 1, "Delete action didn't remove one movie");
+    Contract.affirm(affected == 1, "Delete action didn't remove one movie");
     return result.success(null);
   }
 }
