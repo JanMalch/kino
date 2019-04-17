fdfed34d54a8d9bc80f536e2d074876c097694b6
JanMalch <janmalch@gmail.com>
2019-04-17 14:20:35 +0200

    test: Email exists check in SignUpControl

--- src/test/java/io/github/janmalch/kino/control/SignUpControlTest.java
+++ src/test/java/io/github/janmalch/kino/control/SignUpControlTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.github.janmalch.kino.api.ResponseResultBuilder;
 import io.github.janmalch.kino.api.model.SignUpDto;
+import io.github.janmalch.kino.entity.Account;
+import io.github.janmalch.kino.repository.UserRepository;
 import java.util.HashMap;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +39,27 @@ class SignUpControlTest {
     var problem = (HashMap<String, Object>) response.getEntity(); // TODO: refactor when new mapper
     assertEquals("A required field is empty", problem.get("title"));
     assertEquals("The required field 'lastName' is empty", problem.get("detail"));
+  }
+
+  @Test
+  void executeEmailExists() {
+    var repository = new UserRepository();
+    var existing = new Account();
+    existing.setEmail("existing@example.com");
+    repository.add(existing);
+
+    var data = new SignUpDto();
+    data.setEmail("existing@example.com");
+    data.setFirstName("Test");
+    data.setLastName("Account");
+    data.setPassword("Start123");
+    data.setBirthday("1990-01-01");
+
+    var control = new SignUpControl(data);
+    var response = control.execute(new ResponseResultBuilder<>());
+    assertEquals(400, response.getStatus());
+    var problem = (HashMap<String, Object>) response.getEntity(); // TODO: refactor when new mapper
+    assertEquals(
+        "An account with the email 'existing@example.com' already exists", problem.get("detail"));
   }
 }
