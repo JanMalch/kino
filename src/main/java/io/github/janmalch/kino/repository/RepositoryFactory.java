d263f5adc5937bb517a2394232d74046e616857a
JanMalch <janmalch@gmail.com>
2019-04-26 17:33:14 +0200

    style: prevent RepositoryFactory instantiation

--- src/main/java/io/github/janmalch/kino/repository/RepositoryFactory.java
+++ src/main/java/io/github/janmalch/kino/repository/RepositoryFactory.java
@@ -2,6 +2,8 @@ package io.github.janmalch.kino.repository;
 
 public final class RepositoryFactory {
 
+  private RepositoryFactory() {}
+
   public static <T> Repository<T> createRepository(Class<T> entityType) {
     return new RepositoryImpl<>(entityType);
   }
