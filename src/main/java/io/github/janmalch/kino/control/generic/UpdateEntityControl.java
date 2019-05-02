83acef03bdaa083ec0916f2c89dae50f8ceb89a2
JanMalch <janmalch@gmail.com>
2019-05-02 23:14:09 +0200

    feat: generic controls

---
+++ src/main/java/io/github/janmalch/kino/control/generic/UpdateEntityControl.java
@@ -0,0 +1,39 @@
+package io.github.janmalch.kino.control.generic;
+
+import io.github.janmalch.kino.control.Control;
+import io.github.janmalch.kino.control.ResultBuilder;
+import io.github.janmalch.kino.problem.Problems;
+import io.github.janmalch.kino.repository.Repository;
+import io.github.janmalch.kino.repository.RepositoryFactory;
+import io.github.janmalch.kino.util.Mapping;
+import io.github.janmalch.kino.util.ReflectionMapper;
+
+public class UpdateEntityControl<P, E> implements Control<Void> {
+
+  private final Mapping<P, E> mapper;
+  private final long id;
+  private final P dto;
+  private final Class<E> entityClass;
+  private final Repository<E> repository;
+
+  public UpdateEntityControl(long id, P dto, Class<E> entityClass) {
+    this(id, dto, entityClass, new ReflectionMapper<>());
+  }
+
+  public UpdateEntityControl(long id, P dto, Class<E> entityClass, Mapping<P, E> mapper) {
+    this.id = id;
+    this.dto = dto;
+    this.entityClass = entityClass;
+    this.repository = RepositoryFactory.createRepository(entityClass);
+    this.mapper = mapper;
+  }
+
+  @Override
+  public <T> T execute(ResultBuilder<T, Void> result) {
+    var entity =
+        Problems.requireEntity(repository.find(id), id, "No such " + entityClass.getSimpleName());
+    var update = mapper.update(dto, entity, entityClass);
+    repository.update(update);
+    return result.success(null, entityClass.getSimpleName() + " successfully updated");
+  }
+}
