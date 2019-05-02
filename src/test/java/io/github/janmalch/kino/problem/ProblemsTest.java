f104a10ec40a933e2f003e494f4fb58cfca09b7c
JanMalch <janmalch@gmail.com>
2019-05-02 12:01:16 +0200

    test: requireEntity

--- src/test/java/io/github/janmalch/kino/problem/ProblemsTest.java
+++ src/test/java/io/github/janmalch/kino/problem/ProblemsTest.java
@@ -30,6 +30,37 @@ class ProblemsTest {
       assertEquals("Failed to provide required data", problem.getTitle());
       assertEquals(Response.Status.BAD_REQUEST, problem.getStatus());
       assertEquals("This should be provided", problem.getDetail());
+      assertNotNull(problem.getInstance());
+      return;
+    }
+    fail("Should've thrown a ThrowableProblem");
+  }
+
+  @Test
+  void requireEntity() {
+    try {
+      String x = Problems.requireEntity("example", 5, "no test example found");
+      assertEquals("example", x);
+    } catch (ThrowableProblem e) {
+      fail("Should not throw");
+    }
+  }
+
+  @Test
+  void requireEntityFailing() {
+    try {
+      Problems.requireEntity(null, 5, "No test example found");
+      fail("Should've thrown an exception");
+    } catch (ThrowableProblem e) {
+      assertEquals("No test example found", e.getMessage());
+
+      var problem = e.getProblem();
+      assertTrue(problem.getType().toString().contains("no-such-entity"));
+      assertEquals("Failed to find entity for given ID", problem.getTitle());
+      assertEquals(Response.Status.NOT_FOUND, problem.getStatus());
+      assertEquals("No test example found", problem.getDetail());
+      assertEquals(5L, problem.getParameters().get("id"));
+      assertNotNull(problem.getInstance());
       return;
     }
     fail("Should've thrown a ThrowableProblem");
