64148612672b5e0b32107391e664ff07f16e142c
bko <koll.bernhard@gmail.com>
2019-05-02 17:13:47 +0200

    refactor: removing getter/setter on accountDtos

--- src/main/java/io/github/janmalch/kino/api/model/AccountDto.java
+++ src/main/java/io/github/janmalch/kino/api/model/AccountDto.java
@@ -1,32 +1,12 @@
 package io.github.janmalch.kino.api.model;
 
-import io.github.janmalch.kino.entity.Role;
-
-public class AccountDto extends SignUpDto {
-  private Role role;
-  private long id;
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public Role getRole() {
-    return role;
-  }
-
-  public void setRole(Role role) {
-    this.role = role;
-  }
+public class AccountDto extends AccountInfoDto {
 
   @Override
   public String toString() {
     return "AccountDto{"
         + "id='"
-        + id
+        + getId()
         + '\''
         + ", firstName='"
         + getFirstName()
