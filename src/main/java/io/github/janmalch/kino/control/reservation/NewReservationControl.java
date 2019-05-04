c04a349897c7a7496d0c2fe19d0705d91ff37b4d
JanMalch <janmalch@gmail.com>
2019-05-04 11:12:03 +0200

    feat: validate that seats are available

--- src/main/java/io/github/janmalch/kino/control/reservation/NewReservationControl.java
+++ src/main/java/io/github/janmalch/kino/control/reservation/NewReservationControl.java
@@ -1,6 +1,7 @@
 package io.github.janmalch.kino.control.reservation;
 
 import io.github.janmalch.kino.api.model.ReservationDto;
+import io.github.janmalch.kino.api.model.SeatForPresentationDto;
 import io.github.janmalch.kino.control.Control;
 import io.github.janmalch.kino.control.ResultBuilder;
 import io.github.janmalch.kino.control.generic.NewEntityControl;
@@ -14,11 +15,13 @@ import io.github.janmalch.kino.repository.RepositoryFactory;
 import io.github.janmalch.kino.repository.specification.AccountByEmailSpec;
 import io.github.janmalch.kino.repository.specification.Specification;
 import io.github.janmalch.kino.util.Mapping;
+import io.github.janmalch.kino.util.either.EitherResultBuilder;
 import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.ws.rs.core.Response;
 
 public class NewReservationControl implements Control<Long> {
 
@@ -42,6 +45,43 @@ public class NewReservationControl implements Control<Long> {
   }
 
   Optional<Problem> validate() {
+    // more validation needed?
+    return validateSeatsAvailable();
+  }
+
+  Optional<Problem> validateSeatsAvailable() {
+    var seatsForPresentationControl =
+        new GetSeatsWithStatusControl(reservationDto.getPresentationId());
+    var result = seatsForPresentationControl.execute(new EitherResultBuilder<>());
+    if (result.isFailure()) {
+      return Optional.of(result.getProblem());
+    }
+
+    var requestedSeats = reservationDto.getSeatIds();
+
+    // Überschneidung zwischen den Sitzen der Reservierung und den bereits reservierten Sitzen
+    var intersection =
+        result
+            .getSuccess()
+            .getData()
+            .stream() // Stream mit allen Sitzen für den Saal
+            .filter(SeatForPresentationDto::isTaken) // nur die bereits gebuchten Sitze auswählen
+            .map(SeatForPresentationDto::getId) // ID benötigt zum Vergleich
+            .filter(requestedSeats::contains) // Sitze rausnehmen, die NICHT bereits gebucht sind
+            .collect(Collectors.toSet()); // Schnittmenge bleibt übrig
+
+    if (intersection.size() > 0) {
+      var problem =
+          Problem.builder()
+              .type("new-reservation/seats-taken")
+              .title("Seats in reservation are already taken")
+              .status(Response.Status.BAD_REQUEST)
+              .detail(String.format("%d seat(s) already taken", intersection.size()))
+              .parameter("taken_seats", intersection)
+              .instance()
+              .build();
+      return Optional.of(problem);
+    }
 
     return Optional.empty();
   }
