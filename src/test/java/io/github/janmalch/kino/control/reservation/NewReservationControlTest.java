9a67ef81b8082f605288a5b43fa21a4a53238d69
3xtrem3 <marek.hohmann@t-online.de>
2019-05-04 14:03:38 +0200

    feat: added tests

--- src/test/java/io/github/janmalch/kino/control/reservation/NewReservationControlTest.java
+++ src/test/java/io/github/janmalch/kino/control/reservation/NewReservationControlTest.java
@@ -7,6 +7,7 @@ import io.github.janmalch.kino.entity.*;
 import io.github.janmalch.kino.repository.Repository;
 import io.github.janmalch.kino.repository.RepositoryFactory;
 import io.github.janmalch.kino.util.either.EitherResultBuilder;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -21,33 +22,88 @@ public class NewReservationControlTest {
   private final Repository<Seat> seatRepository = RepositoryFactory.createRepository(Seat.class);
   private final Repository<Account> accountRepository =
       RepositoryFactory.createRepository(Account.class);
+  private final Repository<CinemaHall> cinemaHallRepository =
+      RepositoryFactory.createRepository(CinemaHall.class);
 
-  private final String mail = "example@mail.com";
-  private final int reservingSeatAmount = 3;
+  private final String MAIL = "example@mail.com";
+  private final int NUMBER_OF_SEATS = 5;
+  private CinemaHall cinemaHall;
+  private Presentation presentation;
+  private Set<Seat> seats;
 
-  private Set<Long> seatIds = new HashSet<>();
-  private long presentationId;
+  private CinemaHall createCinemaHall(String name) {
+    CinemaHall cinemaHall = new CinemaHall();
+    cinemaHall.setName(name);
+    return cinemaHall;
+  }
 
-  private Set<Long> getSeatIds(int numberOfSeats, int dtoSeatIdAmount) {
-    Set<Seat> seats = new HashSet<>();
-    for (int i = 0; i < numberOfSeats; i++) {
-      seats.add(new Seat());
+  private Seat createSeat(int seatNumber) {
+    Seat seat = new Seat();
+    seat.setSeatNumber(seatNumber);
+    return seat;
+  }
+
+  private Set<Seat> createSeats(int number) {
+    var seats = new HashSet<Seat>();
+    for (int i = 0; i < number; i++) {
+      seats.add(createSeat(i + 1));
     }
-    seatRepository.add(seats);
-    return seats.stream().limit(dtoSeatIdAmount).map(Seat::getId).collect(Collectors.toSet());
+    return seats;
+  }
+
+  private Presentation createPresentation() {
+    return new Presentation();
   }
 
   @BeforeEach
   public void setUp() {
-    seatIds = getSeatIds(6, reservingSeatAmount);
+    var account = new Account();
+    account.setEmail(MAIL);
+    accountRepository.add(account);
+
+    cinemaHall = createCinemaHall("first");
+
+    seats = createSeats(NUMBER_OF_SEATS);
+    seats.forEach(s -> s.setCinemaHall(cinemaHall));
+    cinemaHall.setSeats(new ArrayList<>(seats));
+
+    presentation = createPresentation();
+    presentation.setCinemaHall(cinemaHall);
+
+    cinemaHallRepository.add(cinemaHall);
+    seats.forEach(seatRepository::add);
+    presentationRepository.add(presentation);
+
+    cinemaHallRepository.getEntityManager().clear();
+    seatRepository.getEntityManager().clear();
+    presentationRepository.getEntityManager().clear();
+    accountRepository.getEntityManager().clear();
+  }
+
+  private Set<Long> getSeatIds(int dtoSeatIdAmount) {
+    return seats.stream().limit(dtoSeatIdAmount).map(Seat::getId).collect(Collectors.toSet());
+  }
+
+  /*  @BeforeEach
+  public void setUp() {
+
+    seats = getSeats(6);
+    seatIds = seats.stream().map(Seat::getId).collect(Collectors.toSet());
+
+    CinemaHall cinemaHall = new CinemaHall();
+    cinemaHall.setSeats(new ArrayList<>(seats));
+    cinemaHallRepository.add(cinemaHall);
+
+
     Presentation presentation = new Presentation();
+    presentation.setCinemaHall(cinemaHall);
     presentationRepository.add(presentation);
     presentationId = presentation.getId();
 
     var account = new Account();
     account.setEmail(mail);
     accountRepository.add(account);
-  }
+  }*/
 
   @AfterEach
   public void tearDown() {
@@ -56,21 +112,60 @@ public class NewReservationControlTest {
   }
 
   @Test
-  public void executeValid() {
+  public void execute_Valid() {
     var reservationRepository = RepositoryFactory.createRepository(Reservation.class);
+
+    var reservingSeatAmount = 3;
+
     var dto = new ReservationDto();
+    dto.setPresentationId(presentation.getId());
+    dto.setSeatIds(getSeatIds(reservingSeatAmount));
 
-    dto.setPresentationId(presentationId);
-    dto.setSeatIds(seatIds);
-
-    var control = new NewReservationControl(mail, dto);
+    var control = new NewReservationControl(MAIL, dto);
     var result = control.execute(new EitherResultBuilder<>());
-    var reservation = reservationRepository.find(result.getSuccess().getData());
 
     assertTrue(result.isSuccess());
+
+    var reservation = reservationRepository.find(result.getSuccess().getData());
     assertEquals(reservingSeatAmount, reservation.getSeats().size());
-    seatRepository.getEntityManager().clear();
-    var updatedSeats = seatIds.stream().map(seatRepository::find).collect(Collectors.toList());
+
+    var updatedSeats =
+        dto.getSeatIds().stream().map(seatRepository::find).collect(Collectors.toList());
     updatedSeats.forEach(seat -> assertEquals(1, seat.getReservations().size()));
+  }
+
+  @Test
+  public void execute_Invalid_SeatId_OutOfRange() {
+    // create Seat ID Set with one ID out of Range
+    var seatIds = getSeatIds(NUMBER_OF_SEATS);
+    seatIds = seatIds.stream().map(id -> id += 1).collect(Collectors.toSet());
+
+    var dto = new ReservationDto();
+    dto.setPresentationId(presentation.getId());
+    dto.setSeatIds(seatIds);
+
+    var control = new NewReservationControl(MAIL, dto);
+    var result = control.execute(new EitherResultBuilder<>());
+
+    assertFalse(result.isSuccess());
+  }
+
+  @Test
+  public void execute_Invalid_SeatId_AlreadyTaken() {
+    var reservingSeatAmount = 3;
+
+    var dto = new ReservationDto();
+    dto.setPresentationId(presentation.getId());
+    dto.setSeatIds(getSeatIds(reservingSeatAmount));
+
+    var control = new NewReservationControl(MAIL, dto);
+    var result = control.execute(new EitherResultBuilder<>());
+
+    assertTrue(result.isSuccess());
+
+    control = new NewReservationControl(MAIL, dto);
+    result = control.execute(new EitherResultBuilder<>());
+
+    assertFalse(result.isSuccess());
   }
 }
