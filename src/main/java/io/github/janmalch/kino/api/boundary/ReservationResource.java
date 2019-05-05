769dd312485c2c6bdc9f0cb304f8445b9a4a6cd1
3xtrem3 <marek.hohmann@t-online.de>
2019-05-05 19:01:31 +0200

    feat: added update (PUT) function to reservation API

--- src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
+++ src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
@@ -2,16 +2,13 @@ package io.github.janmalch.kino.api.boundary;
 
 import io.github.janmalch.kino.api.ResponseResultBuilder;
 import io.github.janmalch.kino.api.model.ReservationDto;
-import io.github.janmalch.kino.control.Control;
 import io.github.janmalch.kino.control.reservation.NewReservationControl;
+import io.github.janmalch.kino.control.reservation.UpdateReservationControl;
 import io.github.janmalch.kino.security.Secured;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -30,8 +27,20 @@ public class ReservationResource {
   @ApiOperation(value = "", response = Long.class)
   public Response newReservation(
       @Context SecurityContext securityContext, ReservationDto reservationDto) {
-    Control<Long> control =
+    var control =
         new NewReservationControl(securityContext.getUserPrincipal().getName(), reservationDto);
+    return control.execute(new ResponseResultBuilder<>());
+  }
+
+  @Path("{id}")
+  @PUT
+  @Secured
+  @RolesAllowed("MODERATOR")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(value = "", response = Long.class)
+  public Response updateReservation(@PathParam("id") long id, ReservationDto reservationDto) {
+    var control = new UpdateReservationControl(id, reservationDto);
     return control.execute(new ResponseResultBuilder<>());
   }
 }
---
+++ src/test/java/io/github/janmalch/kino/control/reservation/UpdateReservationControlTest.java
@@ -0,0 +1,104 @@
+package io.github.janmalch.kino.control.reservation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.github.janmalch.kino.api.model.ReservationDto;
+import io.github.janmalch.kino.entity.*;
+import io.github.janmalch.kino.repository.Repository;
+import io.github.janmalch.kino.repository.RepositoryFactory;
+import io.github.janmalch.kino.util.either.EitherResultBuilder;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UpdateReservationControlTest {
+
+  private final Repository<Presentation> presentationRepository =
+      RepositoryFactory.createRepository(Presentation.class);
+  private final Repository<Seat> seatRepository = RepositoryFactory.createRepository(Seat.class);
+  private final Repository<Account> accountRepository =
+      RepositoryFactory.createRepository(Account.class);
+  private final Repository<CinemaHall> cinemaHallRepository =
+      RepositoryFactory.createRepository(CinemaHall.class);
+
+  private final String MAIL = "example@mail.com";
+  private final int NUMBER_OF_SEATS = 5;
+  private CinemaHall cinemaHall;
+  private Presentation presentation;
+  private Set<Seat> seats;
+
+  @BeforeEach
+  public void setUp() {
+    var account = new Account();
+    account.setEmail(MAIL);
+    accountRepository.add(account);
+
+    cinemaHall = ReservationTestUtil.createCinemaHall("first");
+
+    seats = ReservationTestUtil.createSeats(NUMBER_OF_SEATS);
+    seats.forEach(s -> s.setCinemaHall(cinemaHall));
+    cinemaHall.setSeats(new ArrayList<>(seats));
+
+    presentation = ReservationTestUtil.createPresentation();
+    presentation.setCinemaHall(cinemaHall);
+
+    cinemaHallRepository.add(cinemaHall);
+    seats.forEach(seatRepository::add);
+    presentationRepository.add(presentation);
+
+    cinemaHallRepository.getEntityManager().clear();
+    seatRepository.getEntityManager().clear();
+    presentationRepository.getEntityManager().clear();
+    accountRepository.getEntityManager().clear();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    var ew = new EntityWiper();
+    ew.wipeDB();
+  }
+
+  private Long provideExistingReservationId() {
+    var newReservationDto = new ReservationDto();
+    newReservationDto.setSeatIds(ReservationTestUtil.getNumberOfSeatIds(3, seats));
+    newReservationDto.setPresentationId(presentation.getId());
+
+    var newControl = new NewReservationControl(MAIL, newReservationDto);
+    var newResult = newControl.execute(new EitherResultBuilder<>());
+
+    assertTrue(newResult.isSuccess());
+    return newResult.getSuccess().getData();
+  }
+
+  @Test
+  public void testExecute_Valid() {
+    var reservationRepository = RepositoryFactory.createRepository(Reservation.class);
+    var existingReservationId = provideExistingReservationId();
+    var existingReservation = reservationRepository.find(existingReservationId);
+
+    var updateSeatIds = seats.stream().map(Seat::getId).collect(Collectors.toSet());
+    var reservedSeatIds =
+        existingReservation.getSeats().stream().map(Seat::getId).collect(Collectors.toSet());
+
+    updateSeatIds.removeAll(reservedSeatIds);
+
+    var updateReservationDto = new ReservationDto();
+    updateReservationDto.setSeatIds(updateSeatIds);
+    updateReservationDto.setPresentationId(existingReservation.getPresentation().getId());
+
+    var updateControl =
+        new UpdateReservationControl(existingReservation.getId(), updateReservationDto);
+    var updateResult = updateControl.execute(new EitherResultBuilder<>());
+
+    assertTrue(updateResult.isSuccess());
+
+    reservationRepository.getEntityManager().clear();
+
+    var updatedReservation = reservationRepository.find(existingReservation.getId());
+    assertEquals(existingReservation.getId(), updatedReservation.getId());
+    assertNotEquals(existingReservation.getSeats(), updatedReservation.getSeats());
+  }
+}
