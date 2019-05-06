cdb0a33f8f47dc65c22f7fcb8b90e72fea819dfb
JanMalch <janmalch@gmail.com>
2019-05-06 15:05:03 +0200

    test: fix injection

--- src/test/java/io/github/janmalch/kino/api/boundary/PingResourceTest.java
+++ src/test/java/io/github/janmalch/kino/api/boundary/PingResourceTest.java
@@ -2,13 +2,38 @@ package io.github.janmalch.kino.api.boundary;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
 
-class PingResourceTest {
+class PingResourceTest extends JerseyTest {
+
+  @Mock private Logger logger;
+
+  @InjectMocks private PingResource resource;
+
+  @Override
+  protected Application configure() {
+    MockitoAnnotations.initMocks(this);
+    return new ResourceConfig()
+        .register(PingResource.class)
+        .register(
+            new AbstractBinder() {
+              @Override
+              protected void configure() {
+                bind(logger).to(Logger.class);
+              }
+            });
+  }
 
   @Test
   void ping() {
-    var resource = new PingResource();
     var dto = resource.ping();
     assertNotNull(dto.getResponse());
   }
