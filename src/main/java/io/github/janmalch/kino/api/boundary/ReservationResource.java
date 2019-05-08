e4738c25825f5f9112d47144c25ce1c17fc680bb
Marek <marek.hohmann@gmail.com>
2019-05-08 13:05:53 +0200

    feat: added endpoints

--- src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
+++ src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
@@ -2,8 +2,7 @@ package io.github.janmalch.kino.api.boundary;
 
 import io.github.janmalch.kino.api.ResponseResultBuilder;
 import io.github.janmalch.kino.api.model.ReservationDto;
-import io.github.janmalch.kino.control.reservation.NewReservationControl;
-import io.github.janmalch.kino.control.reservation.UpdateReservationControl;
+import io.github.janmalch.kino.control.reservation.*;
 import io.github.janmalch.kino.security.Secured;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -32,15 +31,65 @@ public class ReservationResource {
     return control.execute(new ResponseResultBuilder<>());
   }
 
+  @Path("my-reservations")
+  @GET
+  @Secured
+  @RolesAllowed("CUSTOMER")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getMyReservations(@Context SecurityContext securityContext) {
+    var control = new GetMyReservationsControl(securityContext.getUserPrincipal().getName());
+    return control.execute(new ResponseResultBuilder<>());
+  }
+
+  @Path("my-reservation/{id}")
+  @PUT
+  @Secured
+  @RolesAllowed("CUSTOMER")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(value = "")
+  public Response updateMyReservation(
+      @Context SecurityContext securityContext,
+      @PathParam("id") long id,
+      ReservationDto reservationDto) {
+    var control =
+        new UpdateMyReservationControl(
+            securityContext.getUserPrincipal().getName(), id, reservationDto);
+    return control.execute(new ResponseResultBuilder<>());
+  }
+
+  @Path("{id}")
+  @GET
+  @Secured
+  @RolesAllowed("MODERATOR")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getReservationById(@PathParam("id") long id) {
+    var control = new GetReservationByIdControl(id);
+    return control.execute(new ResponseResultBuilder<>());
+  }
+
+  @Path("")
+  @GET
+  @Secured
+  @RolesAllowed("MODERATOR")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getAllReservations() {
+    var control = new GetAllReservationsControl();
+    return control.execute(new ResponseResultBuilder<>());
+  }
+
   @Path("{id}")
   @PUT
   @Secured
   @RolesAllowed("MODERATOR")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "", response = Long.class)
-  public Response updateReservation(@PathParam("id") long id, ReservationDto reservationDto) {
-    var control = new UpdateReservationControl(id, reservationDto);
+  @ApiOperation(value = "")
+  public Response updateReservationById(@PathParam("id") long id, ReservationDto reservationDto) {
+    var control = new UpdateReservationByIdControl(id, reservationDto);
     return control.execute(new ResponseResultBuilder<>());
   }
 }
