5b681ce9bee562acbade797bfd0fe31c4677f172
Marek <marek.hohmann@gmail.com>
2019-05-10 10:38:05 +0200

    feat: added missing ApiOperation Tags

--- src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
+++ src/main/java/io/github/janmalch/kino/api/boundary/ReservationResource.java
@@ -2,6 +2,7 @@ package io.github.janmalch.kino.api.boundary;
 
 import io.github.janmalch.kino.api.ResponseResultBuilder;
 import io.github.janmalch.kino.api.model.ReservationDto;
+import io.github.janmalch.kino.api.model.ReservationInfoDto;
 import io.github.janmalch.kino.control.reservation.*;
 import io.github.janmalch.kino.security.Secured;
 import io.swagger.annotations.Api;
@@ -23,7 +24,7 @@ public class ReservationResource {
   @RolesAllowed("CUSTOMER")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "", response = Long.class)
+  @ApiOperation(value = "Returns id of created Reservation", response = Long.class)
   public Response newReservation(
       @Context SecurityContext securityContext, ReservationDto reservationDto) {
     var control =
@@ -37,6 +38,10 @@ public class ReservationResource {
   @RolesAllowed("CUSTOMER")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(
+      value = "Returns users reservations ",
+      response = ReservationInfoDto.class,
+      responseContainer = "List")
   public Response getMyReservations(@Context SecurityContext securityContext) {
     var control = new GetMyReservationsControl(securityContext.getUserPrincipal().getName());
     return control.execute(new ResponseResultBuilder<>());
@@ -48,7 +53,7 @@ public class ReservationResource {
   @RolesAllowed("CUSTOMER")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "")
+  @ApiOperation(value = "Updates users reservation for given ID")
   public Response updateMyReservation(
       @Context SecurityContext securityContext,
       @PathParam("id") long id,
@@ -65,7 +70,7 @@ public class ReservationResource {
   @RolesAllowed("CUSTOMER")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "")
+  @ApiOperation(value = "Deletes users reservation for given ID")
   public Response deleteMyReservationById(
       @PathParam("id") long id, @Context SecurityContext securityContext) {
     var control =
@@ -79,6 +84,7 @@ public class ReservationResource {
   @RolesAllowed("MODERATOR")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(value = "Returns reservation for given ID", response = ReservationInfoDto.class)
   public Response getReservationById(@PathParam("id") long id) {
     var control = new GetReservationByIdControl(id);
     return control.execute(new ResponseResultBuilder<>());
@@ -90,6 +96,10 @@ public class ReservationResource {
   @RolesAllowed("MODERATOR")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiOperation(
+      value = "Returns all reservations",
+      response = ReservationInfoDto.class,
+      responseContainer = "List")
   public Response getAllReservations() {
     var control = new GetAllReservationsControl();
     return control.execute(new ResponseResultBuilder<>());
@@ -101,7 +111,7 @@ public class ReservationResource {
   @RolesAllowed("MODERATOR")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "")
+  @ApiOperation(value = "Updates reservation for given ID")
   public Response updateReservationById(@PathParam("id") long id, ReservationDto reservationDto) {
     var control = new UpdateReservationByIdControl(id, reservationDto);
     return control.execute(new ResponseResultBuilder<>());
@@ -113,7 +123,7 @@ public class ReservationResource {
   @RolesAllowed("MODERATOR")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "")
+  @ApiOperation(value = "Deletes reservation for given ID")
   public Response deleteReservationById(@PathParam("id") long id) {
     var control = new DeleteReservationByIdControl(id);
     return control.execute(new ResponseResultBuilder<>());
