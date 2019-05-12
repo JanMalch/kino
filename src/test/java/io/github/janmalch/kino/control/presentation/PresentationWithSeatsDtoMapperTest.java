b4e000b85a25bf1cf7f99ab30ef9cf54e12c28f6
JanMalch <janmalch@gmail.com>
2019-05-12 20:45:40 +0200

    test: mapper and resource tests

---
+++ src/test/java/io/github/janmalch/kino/control/presentation/PresentationWithSeatsDtoMapperTest.java
@@ -0,0 +1,31 @@
+package io.github.janmalch.kino.control.presentation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.janmalch.kino.entity.CinemaHall;
+import io.github.janmalch.kino.entity.Presentation;
+import io.github.janmalch.kino.repository.RepositoryFactory;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+class PresentationWithSeatsDtoMapperTest {
+
+  @Test
+  void map() {
+    var cinemaHall = new CinemaHall();
+    cinemaHall.setName("Saal 1");
+    RepositoryFactory.createRepository(CinemaHall.class).add(cinemaHall);
+
+    var entity = new Presentation();
+    entity.setDate(new Date());
+    entity.setCinemaHall(cinemaHall);
+
+    RepositoryFactory.createRepository(Presentation.class).add(entity);
+
+    var mapper = new PresentationWithSeatsDtoMapper();
+    var dto = mapper.map(entity);
+
+    assertEquals(entity.getId(), dto.getId());
+    assertEquals(entity.getCinemaHall().getId(), dto.getCinemaHallId());
+  }
+}
