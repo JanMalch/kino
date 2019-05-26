f441b0802c79eacce758d38d34836430b60eb90e
JanMalch <janmalch@gmail.com>
2019-05-26 11:35:45 +0200

    fix: index file for pipes

--- src/main/angular/kino-frontend/src/app/shared/pipes/index.ts
+++ src/main/angular/kino-frontend/src/app/shared/pipes/index.ts
@@ -1,5 +1,5 @@
 export * from './seat-matrix.pipe';
 export * from './seat-color.pipe';
 export * from './parse-date.pipe';
-export * from '@shared/pipes/is-employee.pipe';
-export * from '@shared/pipes/has-role.pipe';
+export * from './is-employee.pipe';
+export * from './has-role.pipe';
