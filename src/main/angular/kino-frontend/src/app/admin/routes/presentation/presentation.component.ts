1de1acc369e1ab25fb9be4e84951d19b4b73a5e2
JanMalch <janmalch@gmail.com>
2019-06-08 11:22:50 +0200

    fix: format date field in presentation form

--- src/main/angular/kino-frontend/src/app/admin/routes/presentation/presentation.component.ts
+++ src/main/angular/kino-frontend/src/app/admin/routes/presentation/presentation.component.ts
@@ -1,4 +1,4 @@
-import {Component, Injectable} from '@angular/core';
+import {Component, Inject, Injectable, LOCALE_ID} from '@angular/core';
 import {CrudService, GenericForm} from "@admin/services";
 import {DefaultService} from "@api/api/default.service";
 import {Observable} from "rxjs";
@@ -7,13 +7,21 @@ import {PresentationWithSeatsDto} from "@api/model/presentationWithSeatsDto";
 import {NewPresentationDto} from "@api/model/newPresentationDto";
 import {PresentationService} from "@core/services";
 import {tap} from "rxjs/operators";
+import {ParseDatePipe} from "@shared/pipes";
+import {DatePipe} from "@angular/common";
 
 @Injectable()
 export class PresentationCrudService implements CrudService<NewPresentationDto, PresentationWithSeatsDto> {
 
+  private readonly parseDatePipe = new ParseDatePipe();
+  private readonly datePipe;
+
   constructor(private api: DefaultService,
-              private presentationService: PresentationService) {
+              private presentationService: PresentationService,
+              @Inject(LOCALE_ID) locale: string) {
+    this.datePipe = new DatePipe(locale);
   }
+
 
   create(dto: NewPresentationDto): Observable<number> {
     return this.api.newPresentation(dto).pipe(
@@ -55,12 +63,16 @@ export class PresentationCrudService implements CrudService<NewPresentationDto, 
 
   transformReadForForm(read: PresentationWithSeatsDto): NewPresentationDto {
     return {
-      date: read.date,
+      date: this.formatAsYYYYMMDDHHmm(read.date) as unknown as Date,
       cinemaHallId: read.cinemaHallId,
       movieId: read.movie.id
-    }
+    };
   }
 
+  private formatAsYYYYMMDDHHmm(date: any): string {
+    const parsed = this.parseDatePipe.transform(date);
+    return this.datePipe.transform(parsed, 'yyyy-MM-dd\'T\'HH:mm:ss');
+  }
 }
 
 
