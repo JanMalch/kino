8903739d5aceb362668fec0b195ee1cf851c3a1c
JanMalch <janmalch@gmail.com>
2019-06-10 18:33:14 +0200

    docs: Control, ManagingControl and ResultBuilder

--- src/main/java/io/github/janmalch/kino/control/Control.java
+++ src/main/java/io/github/janmalch/kino/control/Control.java
@@ -1,5 +1,19 @@
 package io.github.janmalch.kino.control;
 
+/**
+ * Interface for controls in the entity-control-boundary-pattern context. Each control represents a
+ * use-case in the domain. It's logic is defined in the {@link Control#execute} method.
+ *
+ * @param <P> type of the result
+ */
 public interface Control<P> {
+  /**
+   * Executes the control to run the defined use-case logic. Pass in a {@link ResultBuilder}
+   * instance to define the desired result objects.
+   *
+   * @param result a {@link ResultBuilder}, that creates the result objects
+   * @param <T> the type of the result object
+   * @return an instance of a result object, defined by the {@link ResultBuilder}
+   */
   <T> T execute(ResultBuilder<T, P> result);
 }
--- src/main/java/io/github/janmalch/kino/control/ManagingControl.java
+++ src/main/java/io/github/janmalch/kino/control/ManagingControl.java
@@ -5,16 +5,47 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * A class that automatically manages resource, registered within a subclass.
+ *
+ * @param <P> type of the result
+ */
 public abstract class ManagingControl<P> implements Control<P>, Manageable {
 
   private final Set<Manageable> resources = new HashSet<>();
 
+  /**
+   * Executes the control to run the defined use-case logic. Pass in a {@link ResultBuilder}
+   * instance to define the desired result objects. Will be called by the {@link
+   * ManagingControl#execute(ResultBuilder)} method.
+   *
+   * @param result a {@link ResultBuilder}, that creates the result objects
+   * @param <T> the type of the result object
+   * @return an instance of a result object, defined by the {@link ResultBuilder}
+   * @see Control#execute(ResultBuilder)
+   */
   public abstract <T> T compute(ResultBuilder<T, P> result);
 
+  /**
+   * Registers the given resource, to make it managed by the control
+   *
+   * @param resource the resource to manage
+   */
   protected void manage(Manageable... resource) {
     this.resources.addAll(Arrays.asList(resource));
   }
 
+  /**
+   * Executes the control to run the defined use-case logic. Pass in a {@link ResultBuilder}
+   * instance to define the desired result objects. <br>
+   * The actual logic is delegated to the {@link ManagingControl#compute(ResultBuilder)} method.
+   * This method ensures that all resources are freed, by always calling {@link
+   * ManagingControl#close()}
+   *
+   * @param result a {@link ResultBuilder}, that creates the result objects
+   * @param <T> the type of the result object
+   * @return an instance of a result object, defined by the {@link ResultBuilder}
+   */
   @Override
   public final <T> T execute(ResultBuilder<T, P> result) {
     try {
@@ -24,6 +55,7 @@ public abstract class ManagingControl<P> implements Control<P>, Manageable {
     }
   }
 
+  /** Closes all registered resources */
   @Override
   public void close() {
     resources.forEach(Manageable::close);
