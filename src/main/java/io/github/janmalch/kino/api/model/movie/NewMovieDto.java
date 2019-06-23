b50f42a637f5ab7f517caf280085d0d52fc8b3e3
kollb <koll.bernhard@gmail.com>
2019-06-23 15:12:54 +0200

    feat: add constraints

--- src/main/java/io/github/janmalch/kino/api/model/movie/NewMovieDto.java
+++ src/main/java/io/github/janmalch/kino/api/model/movie/NewMovieDto.java
@@ -1,13 +1,16 @@
 package io.github.janmalch.kino.api.model.movie;
 
-public class NewMovieDto extends AbstractMovieDto {
+import javax.validation.constraints.DecimalMin;
 
+public class NewMovieDto extends AbstractMovieDto {
+  @DecimalMin("1.0")
   private long priceCategoryId;
 
   public long getPriceCategoryId() {
     return priceCategoryId;
   }
 
+  @DecimalMin("1.0")
   public void setPriceCategoryId(long priceCategoryId) {
     this.priceCategoryId = priceCategoryId;
   }
